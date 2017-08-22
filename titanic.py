bd70b94b29aaaa89d663b5ca8025a016e03ade43
Albert <lohoi@umich.edu>
2017-08-21 21:57:51 -0700

    simple logreg implementation gets ~65% accuracy on test

--- titanic.py
+++ titanic.py
@@ -1,5 +1,6 @@
-import numpy as np
 import pandas as pd
+from sklearn import linear_model as lm
+import numpy as np
 
 # ************************************************
 # * File Parsing Functions
@@ -7,29 +8,31 @@ import pandas as pd
 def read_files():
     return pd.read_csv('train.csv',sep=','), pd.read_csv('test.csv',sep=',')
 
-def split_solution(train_file):
-    train_y = train_file[train_file.columns[1]]
-    train_X = train_file.drop('Survived', axis=1)
-    return train_X, train_y
+train_data, test_data = read_files();
 
-def convertCategoricalColumns(train_X):
-    # parses the name of the person and replaces according to these codes
-    #   * 0 - catch-all 
-    #   * 1 - Mr.
-    #   * 2 - Mrs.
-    #   * 3 - Miss or Ms.
+# basic
 
-def impute_data(train_X):
-    return train_X
+train_data = train_data.drop(["Name","Cabin","Age","Embarked","Ticket"],axis=1)
+test_data = test_data.drop(["Name","Cabin","Age","Embarked","Ticket"],axis=1)
 
-def transform_data(train_X):
-    return impute_data(convertCategoricalColumns(train_X))
+# train_data.info()
+test_data.info()
+sex_mat = train_data["Sex"]
+train_data = train_data.drop("Sex",axis=1)
+train_data = pd.concat([train_data, pd.get_dummies(sex_mat)],axis=1)
 
-train_file, test_file = read_files();
+train_y = train_data["Survived"].values
+train_X = train_data.drop("Survived",axis=1).values
 
-train_X, train_y = split_solution(train_file)
+clf = lm.LogisticRegressionCV(Cs=1,cv=5,penalty="l2",n_jobs=-1)
+clf.fit(train_X,train_y)
 
-train_X = train_X.as_matrix()
-train_y = train_y.as_matrix()
+sex_mat = test_data["Sex"]
+test_data = test_data.drop("Sex",axis=1)
+test_data = pd.concat([test_data, pd.get_dummies(sex_mat)],axis=1).fillna(0)
 
-train_X = transform_data(train_X)
+result = pd.concat([test_data["PassengerId"],pd.DataFrame(clf.predict(test_data))],axis=1).values
+
+np.savetxt("simple_output.csv", result, delimiter=",",fmt='%i',header="PassengerId,Survived")
+# output = pd.DataFrame(clf.predict(test_data))
+# output.to_csv('simple_output.csv')

68f5bbecd8b12fd50634466ed04f4a8850aab4b6
Albert <lohoi@umich.edu>
2017-08-24 14:43:23 -0700

    using simple mean imputation for the age and using title in the names proved to give a lower score

--- titanic.py
+++ titanic.py
@@ -8,31 +8,66 @@ import numpy as np
 def read_files():
     return pd.read_csv('train.csv',sep=','), pd.read_csv('test.csv',sep=',')
 
+def extract_title(name):
+    title = name.split(',')[1].split('.')[0].strip().lower()
+    if title == "miss" or title == "mme" or title == "mlle":
+        title = "Ms"
+    return title
 train_data, test_data = read_files();
 
-# basic
+train_data = train_data.drop(["Cabin","Embarked","Ticket"],axis=1)
+test_data = test_data.drop(["Cabin","Embarked","Ticket"],axis=1)
 
-train_data = train_data.drop(["Name","Cabin","Age","Embarked","Ticket"],axis=1)
-test_data = test_data.drop(["Name","Cabin","Age","Embarked","Ticket"],axis=1)
-
-# train_data.info()
-test_data.info()
-sex_mat = train_data["Sex"]
+# one hot encode sex variable
+train_data = pd.concat([train_data, pd.get_dummies(train_data["Sex"])],axis=1)
 train_data = train_data.drop("Sex",axis=1)
-train_data = pd.concat([train_data, pd.get_dummies(sex_mat)],axis=1)
 
-train_y = train_data["Survived"].values
-train_X = train_data.drop("Survived",axis=1).values
+# TODO impute age
+# TODO extract title from name
+title_df = pd.DataFrame({"Title":train_data["Name"].apply(extract_title)})
+# print(title_df["Title"].value_counts())
+title_df = pd.get_dummies(title_df["Title"])
+train_data = train_data.drop("Name",axis=1)
+train_data = pd.concat([train_data, title_df],axis=1)
+
+# TODO simple mean imputation
+train_data["Age"].fillna(train_data["Age"].mean(),inplace=True)
+
+
+# train_data = pd.concat([train_data, pd.get_dummies(title_df["Title"])],axis=1)
+# train_data.drop("Name",axis=1)
+
+
+
+
+test_data["Age"].fillna(test_data["Age"].mean(),inplace=True)
+test_data["Fare"].fillna(test_data["Fare"].mean(),inplace=True)
+
+
+train_y = train_data["Survived"]
+train_data = train_data.drop("Survived",axis=1)
+train_X = train_data.as_matrix()
 
 clf = lm.LogisticRegressionCV(Cs=1,cv=5,penalty="l2",n_jobs=-1)
-clf.fit(train_X,train_y)
+clf.fit(train_data,train_y)
 
 sex_mat = test_data["Sex"]
 test_data = test_data.drop("Sex",axis=1)
 test_data = pd.concat([test_data, pd.get_dummies(sex_mat)],axis=1).fillna(0)
 
+# extract title from name
+title_df = pd.DataFrame({"Title":test_data["Name"].apply(extract_title)})
+test_data = test_data.drop("Name",axis=1)
+# print(title_df["Title"].value_counts())
+title_df = pd.get_dummies(title_df["Title"])
+test_data = pd.concat([test_data, title_df],axis=1)
+padding = pd.DataFrame(np.zeros((418,6)))
+# test_data = pd.concat([test_data, padding],axis=1)
+padding.info()
+train_data.info()
+test_data = pd.concat([ test_data, padding],axis=1)
+
+
 result = pd.concat([test_data["PassengerId"],pd.DataFrame(clf.predict(test_data))],axis=1).values
 
 np.savetxt("simple_output.csv", result, delimiter=",",fmt='%i',header="PassengerId,Survived")
-# output = pd.DataFrame(clf.predict(test_data))
-# output.to_csv('simple_output.csv')

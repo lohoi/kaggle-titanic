0be325c76df5e159a0e8eb7d373d12ec09aba678
Albert <lohoi@umich.edu>
2017-08-25 07:57:17 -0700

    65 percent

--- titanic.py
+++ titanic.py
@@ -24,11 +24,11 @@ train_data = train_data.drop("Sex",axis=1)
 
 # TODO impute age
 # TODO extract title from name
-title_df = pd.DataFrame({"Title":train_data["Name"].apply(extract_title)})
-# print(title_df["Title"].value_counts())
-title_df = pd.get_dummies(title_df["Title"])
+# title_df = pd.DataFrame({"Title":train_data["Name"].apply(extract_title)})
+# # print(title_df["Title"].value_counts())
+# title_df = pd.get_dummies(title_df["Title"])
 train_data = train_data.drop("Name",axis=1)
-train_data = pd.concat([train_data, title_df],axis=1)
+# train_data = pd.concat([train_data, title_df],axis=1)
 
 # TODO simple mean imputation
 train_data["Age"].fillna(train_data["Age"].mean(),inplace=True)
@@ -36,9 +36,6 @@ train_data["Age"].fillna(train_data["Age"].mean(),inplace=True)
 
 # train_data = pd.concat([train_data, pd.get_dummies(title_df["Title"])],axis=1)
 # train_data.drop("Name",axis=1)
-
-
-
 
 test_data["Age"].fillna(test_data["Age"].mean(),inplace=True)
 test_data["Fare"].fillna(test_data["Fare"].mean(),inplace=True)
@@ -56,16 +53,16 @@ test_data = test_data.drop("Sex",axis=1)
 test_data = pd.concat([test_data, pd.get_dummies(sex_mat)],axis=1).fillna(0)
 
 # extract title from name
-title_df = pd.DataFrame({"Title":test_data["Name"].apply(extract_title)})
+# title_df = pd.DataFrame({"Title":test_data["Name"].apply(extract_title)})
 test_data = test_data.drop("Name",axis=1)
 # print(title_df["Title"].value_counts())
-title_df = pd.get_dummies(title_df["Title"])
-test_data = pd.concat([test_data, title_df],axis=1)
-padding = pd.DataFrame(np.zeros((418,6)))
+# title_df = pd.get_dummies(title_df["Title"])
+# test_data = pd.concat([test_data, title_df],axis=1)
+# padding = pd.DataFrame(np.zeros((418,6)))
 # test_data = pd.concat([test_data, padding],axis=1)
-padding.info()
-train_data.info()
-test_data = pd.concat([ test_data, padding],axis=1)
+# padding.info()
+# train_data.info()
+# test_data = pd.concat([ test_data, padding],axis=1)
 
 
 result = pd.concat([test_data["PassengerId"],pd.DataFrame(clf.predict(test_data))],axis=1).values
